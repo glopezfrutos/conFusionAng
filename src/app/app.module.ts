322b2a9c871d3a5f9c5c3fd9def5381ffbca48ad
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-01-24 16:17:32 -0300

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,15 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
-import 'hammerjs';
+
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
