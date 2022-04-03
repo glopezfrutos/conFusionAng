e6036ee3911b647289f39a6818d60eb7709680d2
glf86 <gus.lopezfrutos@gmail.com>
2022-04-03 01:27:10 -0300

    Directives

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -39,6 +39,7 @@ import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 
@@ -52,7 +53,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
